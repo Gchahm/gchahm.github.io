6be79eefa9d218bac0b4b457bca2aff2f39b3d06
Gchahm <gustavo.francelino@gmail.com>
2025-06-08 21:07:31 +0000

    Deploying to gh-pages from @ Gchahm/gchahm.github.io@d7bb20032a02655715cdf5697e4b6d9ba1ff1f56 🚀

---
+++ _next/static/ahmrlZhQ99aeMHdnTCIk-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/623-54cb42a329aae3d5.js","static/chunks/pages/index-e07a5d107f829e3f.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
